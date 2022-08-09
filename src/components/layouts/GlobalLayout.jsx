7cbe9c43ed5916027a765736d964285796ee9b66
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-08-09 18:35:35 -0500

    feat: Global layout was added

---
+++ src/components/layouts/GlobalLayout.jsx
@@ -0,0 +1,22 @@
+import { Outlet } from 'react-router-dom'
+import styled from 'styled-components'
+
+import Header from '../views/Header'
+
+export const GlobalLayout = () => {
+  return (
+    <>
+      <nav>
+        <Header />
+      </nav>
+      <Main>
+        <Outlet />
+      </Main>
+    </>
+  )
+}
+
+const Main = styled.main`
+  margin-top: 1rem;
+  padding: 0 1rem;
+`
