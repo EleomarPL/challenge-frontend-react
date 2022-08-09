2715dd0d0f1edcc10def6fef6a2a71c664ec8e39
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-08-09 18:41:45 -0500

    feat: Base routers was added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
+import { Routes, Route } from 'react-router-dom'
+import { GlobalLayout } from '@/components/layouts/GlobalLayout'
+
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Routes>
+        <Route element={ <GlobalLayout /> }>
+          <Route index
+            element={
+              <h2>Characters</h2>
+            }
+          />
+          <Route path="/episodes"
+            element={
+              <h2>Episodes</h2>
+            }
+          />
+          <Route path="/locations"
+            element={
+              <h2>Locations</h2>
+            }
+          />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
