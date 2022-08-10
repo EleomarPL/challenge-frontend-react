25283931e1bf2a073d788f5a24306067d85be2f2
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-08-10 10:22:28 -0500

    feat: Base modals was added

---
+++ src/components/modals/ModalCharacter.jsx
@@ -0,0 +1,49 @@
+import Button from '@mui/material/Button'
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import DialogTitle from '@mui/material/DialogTitle'
+
+import CardDetail from '@/components/cards/CardDetail'
+import PropTypes from 'prop-types'
+
+const ModalCharacter = ({ open, setOpen, row }) => {
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  return (
+    <div>
+      <Dialog
+        open={ open }
+        onClose={ handleClose }
+        scroll="paper"
+        fullWidth={ true }
+        aria-labelledby="scroll-dialog-title"
+        aria-describedby="scroll-dialog-description"
+      >
+        <DialogTitle id="scroll-dialog-title">Character</DialogTitle>
+        <DialogContent dividers={ true }>
+          <CardDetail row={ row }
+            list={
+              [
+                { property: 'Status', value: row.status },
+                { property: 'Specie', value: row.species }
+              ]
+            }
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={ handleClose }>Close</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  )
+}
+ModalCharacter.propTypes = {
+  open: PropTypes.bool.isRequired,
+  setOpen: PropTypes.func.isRequired,
+  row: PropTypes.object.isRequired
+}
+
+export default ModalCharacter
