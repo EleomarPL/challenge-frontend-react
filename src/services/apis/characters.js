d7be05b689c82bfd11c929add0c2441e8849ed0f
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-08-10 12:29:51 -0500

    fix: Add parameter page in get axios

--- src/services/apis/characters.js
+++ src/services/apis/characters.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { BASE_API } from '../BASE_API'
 const CHARACTER_API = `${BASE_API}character`
 
-export const searchCharacterByNameAxios = async (name) => {
-  const response = await axios.get(`${CHARACTER_API}?name=${name}`)
+export const searchCharacterByNameAxios = async (name, page = 0) => {
+  const response = await axios.get(`${CHARACTER_API}?name=${name}&page=${page}`)
   return response.data
 }
