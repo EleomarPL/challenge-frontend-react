16675f80717f997e97f9de57cf591cee88a93925
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-08-10 13:36:54 -0500

    feat: Base reducers added in character slice

--- src/features/charactersSlice.js
+++ src/features/charactersSlice.js
@@ -5,15 +5,42 @@ export const charactersSlice = createSlice({
   initialState: {
     characters: [],
     character: {},
+    info: {},
+    page: 1,
     status: 'idle'
   },
   reducers: {
-    getInitial: state => {
+    requestPending: (state) => {
+      state.status = 'loading'
+    },
+    setNewPage: (state, action) => {
+      state.page = action.payload
+    },
+    setError: (state, action) => {
+      console.log('error')
+      state.status = 'error'
+    },
+    setCharacters: (state, action) => {
+      const results = action.payload.results
+      const currentData = state.characters
+
+      state.characters = [...currentData, ...results]
+      state.info = action.payload.info
+      state.status = 'succeeded'
+    },
+    resetState: (state) => {
       state.characters = []
+      state.character = {}
+      state.info = {}
+      state.page = 1
+      state.status = 'idle'
     }
   }
 })
 
-export const { getInitial } = charactersSlice.actions
+export const {
+  requestPending, setCharacters, setError,
+  setNewPage, resetState
+} = charactersSlice.actions
 
 export default charactersSlice.reducer
