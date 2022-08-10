498c96e887d81252ef3fb070e4d78431a28ab001
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-08-10 16:02:46 -0500

    fix: Repair link logo

--- src/components/views/Header.jsx
+++ src/components/views/Header.jsx
@@ -37,8 +37,8 @@ const ResponsiveAppBar = () => {
           <Typography
             variant="h6"
             noWrap
-            component="a"
-            href="/"
+            component={ Link }
+            to="/"
             sx={ {
               mr: 2,
               display: { xs: 'none', md: 'flex' },
