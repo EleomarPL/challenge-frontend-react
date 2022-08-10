ef154feb673aa3fc0c054b559b18eabf7b0e6c41
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-08-10 14:39:33 -0500

    feat: Hook to control request of episodes data

--- src/hooks/useControlRequest.js
+++ src/hooks/useControlRequest.js
@@ -2,19 +2,28 @@ import { useSelector } from 'react-redux'
 
 import { calculateLimit } from '../utils/calculateLimit'
 import useGetCharacter from './useGetCharacter'
+import useGetEpisode from './useGetEpisode'
 
 const useControlRequest = () => {
-  const currentPage = useSelector(state => state.characters.page)
+  const currentPageCharacter = useSelector(state => state.characters.page)
+  const currentPageEpisode = useSelector(state => state.episodes.page)
+
   const { searchCharactersByName } = useGetCharacter()
+  const { searchEpisodesByName } = useGetEpisode()
 
   const willNewRequestCharacter = (newPage, rowsPerPage, searcher) => {
-    const limit = calculateLimit(newPage, rowsPerPage, currentPage)
-    if (limit) return searchCharactersByName(searcher, currentPage + 1)
+    const limit = calculateLimit(newPage, rowsPerPage, currentPageCharacter)
+    if (limit) return searchCharactersByName(searcher, currentPageCharacter + 1)
+    return false
+  }
+  const willNewRequestEpisode = (newPage, rowsPerPage, searcher) => {
+    const limit = calculateLimit(newPage, rowsPerPage, currentPageEpisode)
+    if (limit) return searchEpisodesByName(searcher, currentPageEpisode + 1)
     return false
   }
 
   return {
-    willNewRequestCharacter
+    willNewRequestCharacter, willNewRequestEpisode
   }
 }
 
