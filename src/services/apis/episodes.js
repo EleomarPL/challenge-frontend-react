c1f627b258c12cf2efa4b47519501af59b5c70b3
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-08-10 14:03:05 -0500

    fix: Added parameter page in episode and location request

--- src/services/apis/episodes.js
+++ src/services/apis/episodes.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { BASE_API } from '../BASE_API'
 const EPISODE_API = `${BASE_API}episode`
 
-export const searchEpisodeByNameAxios = async (name) => {
-  const response = await axios.get(`${EPISODE_API}?name=${name}`)
+export const searchEpisodeByNameAxios = async (name, page = 0) => {
+  const response = await axios.get(`${EPISODE_API}?name=${name}&page=${page}`)
   return response.data
 }
--- src/services/apis/locations.js
+++ src/services/apis/locations.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { BASE_API } from '../BASE_API'
 const LOCATION_API = `${BASE_API}location`
 
-export const searchLocationByNameAxios = async (name) => {
-  const response = await axios.get(`${LOCATION_API}?name=${name}`)
+export const searchLocationByNameAxios = async (name, page = 0) => {
+  const response = await axios.get(`${LOCATION_API}?name=${name}&page=${page}`)
   return response.data
 }
