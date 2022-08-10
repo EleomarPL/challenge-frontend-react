dcb5c3a862485ff7eeca9d7bcc03a3076bf52ba8
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-08-10 15:01:21 -0500

    feat: Hook to control request of location data

--- src/hooks/useControlRequest.js
+++ src/hooks/useControlRequest.js
@@ -3,13 +3,16 @@ import { useSelector } from 'react-redux'
 import { calculateLimit } from '../utils/calculateLimit'
 import useGetCharacter from './useGetCharacter'
 import useGetEpisode from './useGetEpisode'
+import useGetLocation from './useGetLocations'
 
 const useControlRequest = () => {
   const currentPageCharacter = useSelector(state => state.characters.page)
   const currentPageEpisode = useSelector(state => state.episodes.page)
+  const currentPageLocation = useSelector(state => state.locations.page)
 
   const { searchCharactersByName } = useGetCharacter()
   const { searchEpisodesByName } = useGetEpisode()
+  const { searchLocationByName } = useGetLocation()
 
   const willNewRequestCharacter = (newPage, rowsPerPage, searcher) => {
     const limit = calculateLimit(newPage, rowsPerPage, currentPageCharacter)
@@ -21,9 +24,16 @@ const useControlRequest = () => {
     if (limit) return searchEpisodesByName(searcher, currentPageEpisode + 1)
     return false
   }
+  const willNewRequestLocation = (newPage, rowsPerPage, searcher) => {
+    const limit = calculateLimit(newPage, rowsPerPage, currentPageLocation)
+    if (limit) return searchLocationByName(searcher, currentPageLocation + 1)
+    return false
+  }
 
   return {
-    willNewRequestCharacter, willNewRequestEpisode
+    willNewRequestCharacter,
+    willNewRequestEpisode,
+    willNewRequestLocation
   }
 }
 
