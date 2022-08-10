dce3a7d4cc8889dbbc6c184a7a03610ebabe0aec
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-08-10 15:00:48 -0500

    feat: Base reducers added in location slice

--- src/features/locationsSlice.js
+++ src/features/locationsSlice.js
@@ -5,15 +5,45 @@ export const locationsSlice = createSlice({
   initialState: {
     locations: [],
     location: {},
+    info: {},
+    page: 1,
     status: 'idle'
   },
   reducers: {
-    getInitial: state => {
+    selectLocation: (state, action) => {
+      state.location = action.payload
+    },
+    requestPending: (state) => {
+      state.status = 'loading'
+    },
+    setNewPage: (state, action) => {
+      state.page = action.payload
+    },
+    setError: (state, action) => {
+      console.log('error')
+      state.status = 'error'
+    },
+    setLocations: (state, action) => {
+      const results = action.payload.results
+      const currentData = state.locations
+
+      state.locations = [...currentData, ...results]
+      state.info = action.payload.info
+      state.status = 'succeeded'
+    },
+    resetState: (state) => {
       state.locations = []
+      state.location = {}
+      state.info = {}
+      state.page = 1
+      state.status = 'idle'
     }
   }
 })
 
-export const { getInitial } = locationsSlice.actions
+export const {
+  selectLocation, requestPending, setNewPage,
+  setError, setLocations, resetState
+} = locationsSlice.actions
 
 export default locationsSlice.reducer
