4822a897dc1fde2f8837d6bd8d0f40e5e3e0c2ea
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-08-10 09:52:07 -0500

    feat: Searcher component was added

---
+++ src/components/views/Searcher.jsx
@@ -0,0 +1,39 @@
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
+import SearchIcon from '@mui/icons-material/Search'
+
+import PropTypes from 'prop-types'
+import { debounce } from 'lodash'
+import { useEffect, useMemo } from 'react'
+
+const Searcher = ({ type = 'Character', setState = () => {} }) => {
+  const handleChange = (event) => {
+    setState(event.target.value)
+  }
+  const debounceHandleChange = useMemo(
+    () => debounce(handleChange, 1000)
+    , []
+  )
+  useEffect(() => {
+    return () => {
+      debounceHandleChange.cancel()
+    }
+  }, [])
+
+  return (
+    <Box sx={ { display: 'flex', alignItems: 'flex-end', justifyContent: 'center' } }>
+      <SearchIcon sx={ { color: 'action.active', mr: 1, my: 0.5 } } />
+      <TextField id="input-with" label={ `Search ${type}` }
+        sx={ { width: '80%' } }
+        variant="standard"
+        onChange={ debounceHandleChange } autoFocus
+      />
+    </Box>
+  )
+}
+Searcher.propTypes = {
+  type: PropTypes.string,
+  setState: PropTypes.func
+}
+
+export default Searcher
