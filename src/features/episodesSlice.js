e5144d53d4c2b9b553e006ff26335312ac7c94cc
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-08-10 14:38:56 -0500

    feat: Base reducers added in episode slice

--- src/features/episodesSlice.js
+++ src/features/episodesSlice.js
@@ -5,15 +5,45 @@ export const episodesSlice = createSlice({
   initialState: {
     episode: {},
     episodes: [],
+    info: {},
+    page: 1,
     status: 'idle'
   },
   reducers: {
-    getInitial: state => {
+    selectEpisode: (state, action) => {
+      state.episode = action.payload
+    },
+    requestPending: (state) => {
+      state.status = 'loading'
+    },
+    setNewPage: (state, action) => {
+      state.page = action.payload
+    },
+    setError: (state, action) => {
+      console.log('error')
+      state.status = 'error'
+    },
+    setEpisodes: (state, action) => {
+      const results = action.payload.results
+      const currentData = state.episodes
+
+      state.episodes = [...currentData, ...results]
+      state.info = action.payload.info
+      state.status = 'succeeded'
+    },
+    resetState: (state) => {
       state.episodes = []
+      state.episode = {}
+      state.info = {}
+      state.page = 1
+      state.status = 'idle'
     }
   }
 })
 
-export const { getInitial } = episodesSlice.actions
+export const {
+  selectEpisode, requestPending, setNewPage,
+  setError, setEpisodes, resetState
+} = episodesSlice.actions
 
 export default episodesSlice.reducer
